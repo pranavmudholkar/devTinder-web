be098e24a7170953fcf86d7307ac8db6c8c63501
Pranav Mudholkar <pranavmudholkar455@gmail.com>
2025-09-14 12:56:56 +0530

    Neccesary checks

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
